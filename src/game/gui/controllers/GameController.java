04b4234330bdefd4fd70de7692d5e44dd8557316
ahmedkhlearn-alt <ahmed.khlearn@gmail.com>
2026-05-16 14:34:48 +0300

    board class modification

---
+++ src/game/gui/controllers/GameController.java
@@ -0,0 +1,5 @@
+package game.gui.controllers;
+
+public class GameController {
+
+}
---
+++ src/game/gui/views/BoardView.java
@@ -0,0 +1,11 @@
+package game.gui.views;
+
+import javafx.scene.layout.*;
+import game.engine.Role;
+import game.engine.cells.*;
+
+public class BoardView {
+	private GridPane grid;
+	private StackPane[] cellViews = new StackPane[100];
+	
+}
