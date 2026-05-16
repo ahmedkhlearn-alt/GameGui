1213f5a4af0dcd89a3a2779b0cd11ff7805a9463
ahmedkhlearn-alt <ahmed.khlearn@gmail.com>
2026-05-16 14:59:29 +0300

    board class modification

--- src/game/gui/views/BoardView.java
+++ src/game/gui/views/BoardView.java
@@ -1,5 +1,6 @@
 package game.gui.views;
 
+import javafx.geometry.Insets;
 import javafx.scene.layout.*;
 import game.engine.Role;
 import game.engine.cells.*;
@@ -8,4 +9,25 @@ public class BoardView {
 	private GridPane grid;
 	private StackPane[] cellViews = new StackPane[100];
 	
+	public BoardView()
+	{
+        this.grid = new GridPane();
+        this.grid.setHgap(3);
+        this.grid.setVgap(3);
+        this.grid.getStyleClass().add("board-background");
+        this.grid.setPadding(new Insets(12));//try to remove and add
+        
+        setupGridConstraints();
+        buildEmptyCells();
+	}
+	
+	public GridPane getGrid()
+	{
+		return grid;
+	}
+	
+	public void setupGridConstraints()
+	{
+		
+	}
 }
