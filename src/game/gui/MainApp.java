c320fece263f1fd704ee1c222b4b10f0ede1b3b1
sal0906 <salmahusseinyehia@gmail.com>
2026-05-16 23:49:21 +0300

    updates

--- src/game/gui/MainApp.java
+++ src/game/gui/MainApp.java
@@ -1,6 +1,24 @@
 package game.gui;
+import game.gui.controllers.StartScreenController;
+import game.gui.views.MonsterPanel;
+import game.gui.views.StartScreen;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import game.gui.AppColors;
 
-public class MainApp {
-	int x;
+public class MainApp extends Application {
 
+	@Override
+	public void start(Stage stage) throws Exception {
+	        new StartScreenController(stage).show();
+	    }
+
+	    public static void main(String[] args) {
+	        launch(args);
+	    }
 }
+	
+
+
